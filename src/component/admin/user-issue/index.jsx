02a2c93b551296592ceacdfad20917bba4990731
anurag-17 <anurag.pandey17@gmail.com>
2024-01-16 19:42:14 +0530

    resolve

--- src/component/admin/user-issue/index.jsx
+++ src/component/admin/user-issue/index.jsx
@@ -6,6 +6,7 @@ import DeleteModule from "./delete-modal";
 import Loader from "../../loader";
 import Link from "next/link";
 import { ToastContainer, toast } from "react-toastify";
+import Pagination from "../../pagination";
 
 const UserIssue = () => {
   const auth_token = JSON.parse(localStorage.getItem("accessToken" || ""));
@@ -14,6 +15,15 @@ const UserIssue = () => {
   const [isRefresh, setRefresh] = useState(false);
   const [issueID, setIssueId] = useState("");
   const [isLoader, setLoader] = useState(false);
+  const [limit, setLimit] = useState(10);
+  const [current_page, setCurrentPage] = useState(1);
+  const [total_pages, setTotalPages] = useState(1);
+  const [allResolvedIssue, setAllResolvedIssue] = useState([]);
+  const [selectedFilter, setSelectedFilter] = useState("all");
+  const [isResolve, setIsResolve] = useState("");
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   function closeModal() {
     setOpenDelete(false);
@@ -30,15 +40,20 @@ const UserIssue = () => {
     setOpenDelete(true);
   }
 
+  // --------get All Event Issue----------
   useEffect(() => {
-    defaultGetIssue();
-  }, [!isRefresh]);
+    defaultGetIssue(current_page, limit);
+  }, [current_page, !isRefresh]);
 
-  const defaultGetIssue = () => {
+  const defaultGetIssue = (page, limit) => {
     setLoader(true);
     const option = {
       method: "GET",
       url: "/api/issue/getAllEventIssues",
+      params: {
+        page: page,
+        limit: limit,
+      },
       headers: {
         authorization: auth_token,
       },
@@ -46,12 +61,102 @@ const UserIssue = () => {
     axios
       .request(option)
       .then((response) => {
+        // refreshData();
         setGetUserIssue(response?.data?.eventIssues);
         setLoader(false);
+        setTotalPages(response?.data?.total_pages || 1);
       })
       .catch((error) => {
         console.log("Error", error);
       });
+  };
+
+  // -------------Resolved Events-----------
+  // useEffect(()=>{
+  //   handleAllResolved();
+  // },[]);
+
+  // const handleAllResolved = async () => {
+  //   try {
+  //     const option = {
+  //       method: "GET",
+  //       url: `/api/issue/getAllEventIssues?isResolved=${isResolve}`,
+  //       headers: {
+  //         authorization: auth_token,
+  //       },
+  //     };
+  //     axios.request(option).then((response) => {
+  //       console.log("All Resolved Issue", response?.data);
+  //       // refreshData();
+  //       setAllResolvedIssue(response?.data?.eventIssues || []);
+  //       setTotalPages(response?.data?.total_pages || 1);
+  //     });
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // ----------All event Issue resolve----------
+  const handleResolve = async (id, isResolve) => {
+    try {
+      const option = {
+        method: "PUT",
+        url: `/api/issue/updateEventIssue`,
+        headers: {
+          "Content-Type": "application/json",
+          authorization: auth_token,
+        },
+        data: {
+          _id: id,
+          isResolved: isResolve,
+        },
+      };
+      const response = await axios(option);
+      if (response.status == 200) {
+        console.log(response);
+        refreshData();
+      } else {
+        throw new Error("Failed to resolve");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedFilter === "all") {
+      defaultGetIssue(current_page, limit);
+    } else if (selectedFilter === "resolved") {
+      // handleAllResolved();
+    }
+  }, [current_page, limit, isRefresh, selectedFilter]);
+
+  const handleFilterChange = async(value) => {
+    setLoader(true)
+    if (value === "") {
+      setSelectedFilter("all");
+    } else {
+      setSelectedFilter("resolved");
+      try {
+        const option = {
+          method: "GET",
+          url: `/api/issue/getAllEventIssues?isResolved=${value}`,
+          headers: {
+            authorization: auth_token,
+          },
+        };
+        await axios.request(option).then((response) => {
+          console.log("All Resolved Issue", response?.data);
+          // refreshData();
+          setAllResolvedIssue(response?.data?.eventIssues || []);
+          setTotalPages(response?.data?.total_pages || 1);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    setIsResolve(value);
+    setLoader(false)
   };
 
   return (
@@ -61,69 +166,159 @@ const UserIssue = () => {
         <div className="mt-2 lg:mt-3 xl:mt-4 2xl:mt-7 flex justify-between items-center 2xl:pt-4 2xl:px-10 border ml-10 mr-4 lg:mx-8  bg-white rounded-lg   2xl:h-[100px] xl:h-[70px] lg:h-[60px] md:h-[50px] sm:h-[45px] h-[45px]  xl:px-8 lg:px-5 md:px-4 sm:px-4 px-1 2xl:text-2xl xl:text-[18px] lg:text-[16px] md:text-[15px] sm:text-[14px] text-[13px]">
           <h2 className="font-semibold">Users Issue List </h2>
 
-          <div className="flex items-center w-[40%]">
-            <input
-              type="search"
-              className=" border border-gray-500 p-[2px] lg:p-[4px] 2xl:p-3 rounded-lg  w-11/12 focus:outline-none text-black"
-              placeholder="Search"
-              aria-label="Search"
-              aria-describedby="button-addon1"
-            />
+          <div className="">
+            <label className="text-[14px]">Issues:</label>
+            <select
+              className="text-[12px] rounded-sm border border-gray-300 ml-2"
+              value={isResolve}
+              onChange={(e) => handleFilterChange(e.target.value)}
+            >
+              <option className="text-[12px]" value="">
+                All
+              </option>
+              <option className="text-[12px]" value="true">Resolved</option>
+              <option className="text-[12px]" value="false">Not Resolved</option>
+            </select>
           </div>
+
           <h2>Welcome Back, Admin</h2>
         </div>
-        {Array.isArray(getUserIssue) &&
-          getUserIssue?.map((item) => (
-            <div className="border bg-white sm:ml-10 sm:mr-4 lg:mx-8 rounded-md p-3 my-4 flex flex-col sm:flex-row mx-auto justify-around ">
-              <div className="w-2/12">
-                <Link href={`/eventIssue/${item?._id}`} target="_blank">
-                  <img
-                    src={item?.event?.images[0]?.url}
-                    alt="eventImage"
-                    className="my-3 w-14 sm:w-16 md:w-20 lg:w-16 xl:w-20 2xl:w-28"
-                  />
-                </Link>
-              </div>
-              <div className="w-5/12 ">
-                <h1 className="my-1 font-bold h1_text">{item?.event?.name}</h1>
-                <h1 className="my-1  h1_text">
-                  Location : {item?.event?.location}
-                </h1>
-
-                <h1 className="my-1  h1_text">City : {item?.event?.city}</h1>
-                <h1 className="my-1  h1_text">Issue : {item?.issue}</h1>
-              </div>
-              <div className="w-4/12">
-                <h1 className="my-1  h1_text font-bold">Raised by :</h1>
-                <h1 className="my-1  h1_text">
-                  Name : {item?.userId?.firstname} {item?.userId?.lastname}
-                </h1>
-                <h1 className="my-1  h1_text">
-                  Email : {item?.userId?.email}{" "}
-                </h1>
-                <h1 className="my-1  h1_text">
-                  Mobile : {item?.userId?.mobile}{" "}
-                </h1>
-              </div>
-
-              <div className="w-1/12 col-span-1 my-auto  h1_text">
-                <div>
-                  <button className="border p-1 rounded-md my-1 border-sky-400 text-sky-700 hover:bg-sky-200">
-                    Resolve
-                  </button>
+        {Array.isArray(
+          selectedFilter === "resolved" ? allResolvedIssue : getUserIssue
+        ) && selectedFilter === "resolved"
+          ? allResolvedIssue?.map((item) => (
+              <div className="border bg-white sm:ml-10 sm:mr-4 lg:mx-8 rounded-md p-3 my-4 flex flex-col sm:flex-row mx-auto justify-around ">
+                <div className="w-2/12">
+                  <Link href={`/eventIssue/${item?._id}`} target="_blank">
+                    <img
+                      src={item?.event?.images[0]?.url}
+                      alt="eventImage"
+                      className="my-3 w-14 sm:w-16 md:w-20 lg:w-16 xl:w-20 2xl:w-28"
+                    />
+                  </Link>
                 </div>
-                <div>
-                  <button
-                    onClick={() => openModal(item?._id)}
-                    className="border p-1 rounded-md my-1  border-red-700 text-red-700 hover:bg-red-200 "
-                  >
-                    Delete
-                  </button>
+                <div className="w-5/12 ">
+                  <h1 className="my-1 font-bold h1_text">
+                    {item?.event?.name}
+                  </h1>
+                  <h1 className="my-1  h1_text">
+                    Location : {item?.event?.location}
+                  </h1>
+
+                  <h1 className="my-1  h1_text">City : {item?.event?.city}</h1>
+                  <h1 className="my-1  h1_text">Issue : {item?.issue}</h1>
+                </div>
+                <div className="w-4/12">
+                  <h1 className="my-1  h1_text font-bold">Raised by :</h1>
+                  <h1 className="my-1  h1_text">
+                    Name : {item?.userId?.firstname} {item?.userId?.lastname}
+                  </h1>
+                  <h1 className="my-1  h1_text">
+                    Email : {item?.userId?.email}{" "}
+                  </h1>
+                  <h1 className="my-1  h1_text">
+                    Mobile : {item?.userId?.mobile}{" "}
+                  </h1>
+                </div>
+
+                <div className="w-1/12 col-span-1 my-auto  h1_text">
+                  <div className="mb-2">
+                    {item?.isResolved ? (
+                      <span className="border p-1 rounded-md my-1   border-green-400 text-green-700 ">
+                        Resolved
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => {
+                          handleResolve(item?._id, true);
+                        }}
+                        className="border p-1 rounded-md my-1 border-sky-400 text-sky-700 hover:bg-sky-200"
+                      >
+                        Resolve
+                      </button>
+                    )}
+                  </div>
+                  <div>
+                    <button
+                      onClick={() => openModal(item?._id)}
+                      className="border p-1 rounded-md my-1  border-red-700 text-red-700 hover:bg-red-200 "
+                    >
+                      Delete
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          : getUserIssue?.map((item) => (
+              <div className="border bg-white sm:ml-10 sm:mr-4 lg:mx-8 rounded-md p-3 my-4 flex flex-col sm:flex-row mx-auto justify-around ">
+                <div className="w-2/12">
+                  <Link href={`/eventIssue/${item?._id}`} target="_blank">
+                    <img
+                      src={item?.event?.images[0]?.url}
+                      alt="eventImage"
+                      className="my-3 w-14 sm:w-16 md:w-20 lg:w-16 xl:w-20 2xl:w-28"
+                    />
+                  </Link>
+                </div>
+                <div className="w-5/12 ">
+                  <h1 className="my-1 font-bold h1_text">
+                    {item?.event?.name}
+                  </h1>
+                  <h1 className="my-1  h1_text">
+                    Location : {item?.event?.location}
+                  </h1>
+
+                  <h1 className="my-1  h1_text">City : {item?.event?.city}</h1>
+                  <h1 className="my-1  h1_text">Issue : {item?.issue}</h1>
+                </div>
+                <div className="w-4/12">
+                  <h1 className="my-1  h1_text font-bold">Raised by :</h1>
+                  <h1 className="my-1  h1_text">
+                    Name : {item?.userId?.firstname} {item?.userId?.lastname}
+                  </h1>
+                  <h1 className="my-1  h1_text">
+                    Email : {item?.userId?.email}{" "}
+                  </h1>
+                  <h1 className="my-1  h1_text">
+                    Mobile : {item?.userId?.mobile}{" "}
+                  </h1>
+                </div>
+
+                <div className="w-1/12 col-span-1 my-auto  h1_text">
+                  <div className="mb-2">
+                    {item?.isResolved ? (
+                      <span className="border p-1 rounded-md my-1   border-green-400 text-green-700 ">
+                        Resolved
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => {
+                          handleResolve(item?._id, true);
+                        }}
+                        className="border p-1 rounded-md my-1 border-sky-400 text-sky-700 hover:bg-sky-200"
+                      >
+                        Resolve
+                      </button>
+                    )}
+                  </div>
+                  <div>
+                    <button
+                      onClick={() => openModal(item?._id)}
+                      className="border p-1 rounded-md my-1  border-red-700 text-red-700 hover:bg-red-200 "
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
       </div>
+      <Pagination
+        total_pages={total_pages}
+        current_page={current_page}
+        onPageChange={handlePageChange}
+      />
+
       <Transition appear show={isOpenDelete} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
           <Transition.Child
