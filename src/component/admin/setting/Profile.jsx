931b15ce3428465b79838b388ce3a1a4ed0ff06e
anurag.pandey17 <anurag.pandey17@gmail.com>
2024-01-18 18:33:50 +0530

    settings

--- src/component/admin/setting/Profile.jsx
+++ src/component/admin/setting/Profile.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import Image from "next/image";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useAuth } from "../../../contexts/AuthContext";
 
 const Profile = () => {
+  
+  const [verificationResult, setVerificationResult] = useState(null);
+  const { adminAuthToken } = useAuth();
+    const verifyUserToken = async () => {
+      try {
+        const res = await axios.get(`/api/auth/verifyUserToken/${adminAuthToken}`);
+
+        console.log(res)
+        setVerificationResult(res?.data?.data); // Assuming the API returns some data
+      } catch (error) {
+        console.error('Error verifying user token:', error);
+        // Handle errors if needed
+      }
+    };
+
+  useEffect(() => {
+    verifyUserToken();
+  }, []);
+  
   return (
     <div className="bg-[white] w-full h-full">
       <div className="py-6 px-10 shadow rounded max-w-[400px]  my-4">
@@ -9,18 +31,18 @@ const Profile = () => {
           <Image src="/images/profile.svg" alt="profile" height={100} width={100}/>
         </div>
 
-        <div className="flex gap-5 flex-col justify-center text-[16px] px-10 ">
-          <div className="flex justify-between">
-            <p className="">Name :</p>
-            <p className="font-bold">Admin</p>
+        <div className="flex gap-5 flex-col justify-center text-[16px]  ">
+          <div className="flex  flex-col  sm:flex-row gap-3 justify-start">
+            <p className="w-[100px]">Name :</p>
+            <p className="font-bold">{verificationResult?.firstname}  {verificationResult?.lastname}</p>
           </div>
-          <div className="flex justify-between">
-            <p className="">Email :</p>
-            <p className="font-bold">admin@gmail.com</p>
+          <div className="flex  flex-col  sm:flex-row gap-3 justify-start">
+            <p className="w-[100px]">Email :</p>
+            <p className="font-bold">{verificationResult?.email}</p>
           </div>
-          <div className="flex justify-between">
-            <p className="">Number :</p>
-            <p className="font-bold">7896543210</p>
+          <div className="flex  flex-col  sm:flex-row gap-3 justify-start">
+            <p className="w-[100px]">Number :</p>
+            <p className="font-bold">{verificationResult?.mobile}</p>
           </div>
         </div>
       </div>
