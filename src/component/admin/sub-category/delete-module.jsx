00a144fef614e2b58eb9fb95eb311e6613e4d055
Anurag Pandey <anurag.pandey17@gmail.com>
2024-01-22 16:58:07 +0530

    update

--- src/component/admin/sub-category/delete-module.jsx
+++ src/component/admin/sub-category/delete-module.jsx
@@ -43,7 +43,7 @@ const DeleteModuleC = ({ categoryID, closeModal, refreshData }) => {
   return (
     <>
       <div className="mt-2">
-        <p className="text-[16px] font-normal leading-[30px] text-gray-500 mt-4">
+        <p className="custom_table_text font-normal  text-gray-500 mt-4">
           Do you really want to delete these records? You can't view this in
           your list anymore if you delete!
         </p>
@@ -52,15 +52,18 @@ const DeleteModuleC = ({ categoryID, closeModal, refreshData }) => {
       <div className="mt-8">
         <div className="flex justify-between gap-x-5">
           <button
-            className="w-full border rounded-md text-sm px-2 py-3
-                        hover:bg-lightBlue-200 hover:border-none"
+            className="w-full border border-1 rounded-md border-lightBlue-400 text-lightBlue-700 hover:bg-lightBlue-200 text-sm  sm:px-2 sm:py-3
+                              hover:border-none  border-sky-400 text-sky-700 hover:bg-sky-200"
             onClick={handleClose}
           >
             No, Keep It
           </button>
 
           <button
-            className={`w-full border rounded-md text-sm px-2 py-3
+            className={`w-full border border-1 rounded-md 
+                              text-sm 
+                              border-red-400 text-red-700 bg-red-200  sm:px-2 sm:py-3
+                              hover:border-none
                         ${isLoading ? 'bg-gray-200' : 'hover:bg-red-200'}
                         hover:border-none`}
             onClick={handleDelete}
